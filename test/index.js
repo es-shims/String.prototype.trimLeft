9cb30af808c2cfb3efdc210bbba75cae808528bb
Jordan Harband <ljharb@gmail.com>
2015-07-29 14:14:45 -0700

    Tests

---
+++ test/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var trimLeft = require('../');
+var test = require('tape');
+
+test('as a function', function (t) {
+	t.test('bad array/this value', function (st) {
+		st.throws(function () { trimLeft(undefined, 'a'); }, TypeError, 'undefined is not an object');
+		st.throws(function () { trimLeft(null, 'a'); }, TypeError, 'null is not an object');
+		st.end();
+	});
+
+	require('./tests')(trimLeft, t);
+
+	t.end();
+});
