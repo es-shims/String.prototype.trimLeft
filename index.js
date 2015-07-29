75e7100a865de1748a9f1fa692cc26e0f5a5f9f6
Jordan Harband <ljharb@gmail.com>
2015-07-29 14:23:26 -0700

    Implementation.

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var bind = require('function-bind');
+var define = require('define-properties');
+var replace = bind.call(Function.call, String.prototype.replace);
+
+var leftWhitespace = /^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]*/;
+
+var trimLeft = function trimLeft() {
+	return replace(this, leftWhitespace, '');
+};
+
+var boundTrimLeft = bind.call(Function.call, trimLeft);
+define(boundTrimLeft, {
+	shim: function shimTrimLeft() {
+		if (!String.prototype.trimLeft) {
+			define(String.prototype, { trimLeft: trimLeft });
+		}
+		return String.prototype.trimLeft;
+	}
+});
+
+module.exports = boundTrimLeft;
