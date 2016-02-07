0727d3817db745b9df48ee0e432b035e0eed0b26
Jordan Harband <ljharb@gmail.com>
2016-02-06 16:55:44 -0800

    [Breaking] conform to the es-shim API.

--- index.js
+++ index.js
@@ -2,25 +2,17 @@
 
 var bind = require('function-bind');
 var define = require('define-properties');
-var replace = bind.call(Function.call, String.prototype.replace);
 
-var leftWhitespace = /^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]*/;
+var implementation = require('./implementation');
+var getPolyfill = require('./polyfill');
+var shim = require('./shim');
 
-var trimLeft = function trimLeft() {
-	return replace(this, leftWhitespace, '');
-};
+var bound = bind.call(Function.call, getPolyfill());
 
-var boundTrimLeft = bind.call(Function.call, trimLeft);
-define(boundTrimLeft, {
-	shim: function shimTrimLeft() {
-		var zeroWidthSpace = '\u200b';
-		define(String.prototype, { trimLeft: trimLeft }, {
-			trimLeft: function () {
-				return zeroWidthSpace.trimLeft() !== zeroWidthSpace;
-			}
-		});
-		return String.prototype.trimLeft;
-	}
+define(bound, {
+	getPolyfill: getPolyfill,
+	implementation: implementation,
+	shim: shim
 });
 
-module.exports = boundTrimLeft;
+module.exports = bound;
