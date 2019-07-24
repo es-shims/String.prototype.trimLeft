f59701c7024170e10cb2362cee60ed4032d17fef
Jordan Harband <ljharb@gmail.com>
2019-07-24 14:58:18 -0700

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `covert`, `tape`

--- test/index.js
+++ test/index.js
@@ -7,8 +7,8 @@ var runTests = require('./tests');
 
 test('as a function', function (t) {
 	t.test('bad array/this value', function (st) {
-		st.throws(function () { trimLeft(undefined, 'a'); }, TypeError, 'undefined is not an object');
-		st.throws(function () { trimLeft(null, 'a'); }, TypeError, 'null is not an object');
+		st['throws'](function () { trimLeft(undefined, 'a'); }, TypeError, 'undefined is not an object');
+		st['throws'](function () { trimLeft(null, 'a'); }, TypeError, 'null is not an object');
 		st.end();
 	});
 
